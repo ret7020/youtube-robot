595205a2dbebfe59b64e342b9a4870cd11617d6a
ret7020 <55328925+ret7020@users.noreply.github.com>
2021-05-06 10:57:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ while True:
     data = requests.get("https://www.googleapis.com/youtube/v3/channels?id=" + channel_id + "&part=statistics&key=" + api_key).json()
     print(data)
     print(data['items'][0]['statistics']['viewCount'])
-    im =  Image.open(r"Путь к фону")
-    font = ImageFont.truetype(r'Путь к шрифтам', size=68) #Мой шрифт в папке font
+    im =  Image.open(r"images/background.jpg")
+    font = ImageFont.truetype(r'fonts/8693.ttf', size=68) #Мой шрифт в папке font
     draw_text = ImageDraw.Draw(im)
     draw_text.text(
         (100, 100),
         'Подписчиков: ' + data['items'][0]['statistics']['subscriberCount'] + "\nПросмотров: " + data['items'][0]['statistics']['viewCount'] + "\nВсего видосов: " + data['items'][0]['statistics']['videoCount'],
         font=font,
         fill='red')
-    im.save(r"ПУТЬ ГДЕ СОХРАНЯТЬ", "PNG")
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, r'ПУТЬ ГДЕ СОХРАНИЛИ ФОН', 0)
+    im.save(r"result.png", "PNG")
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, os.path.join(os.getcwd(), "result.png"), 0)
     time.sleep(15)
 #Для установки необходимых библиотек: pip install Pillow requests
